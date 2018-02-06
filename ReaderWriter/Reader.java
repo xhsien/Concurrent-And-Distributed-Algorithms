c6798846d065c68bf6e0796f6e68cf7644f3d66d
chrisboo <christopherboo66@gmail.com>
2018-02-07 07:46:49 +0800

    update sleep function to support random duration

---
+++ ReaderWriter/Reader.java
@@ -0,0 +1,27 @@
+package synchronization;
+
+import utils.Util;
+
+public abstract class Reader implements Runnable {
+
+    protected int id;
+    protected Driver driver;
+
+    public Reader(int id, Driver driver) {
+        this.id = id;
+        this.driver = driver;
+    }
+
+    public abstract void startRead();
+    public abstract void endRead();
+
+    public void run() {
+        while (true) {
+            startRead();
+            System.out.println("Reader " + id + " is reading.");
+            Util.sleep(500, 1000);
+            endRead();
+            Util.sleep(500, 1000);
+        }
+    }
+}
---
+++ ReaderWriter/Writer.java
@@ -0,0 +1,27 @@
+package synchronization;
+
+import utils.Util;
+
+public abstract class Writer implements Runnable {
+
+    protected int id;
+    protected Driver driver;
+
+    public Writer(int id, Driver driver) {
+        this.id = id;
+        this.driver = driver;
+    }
+
+    public abstract void startWrite();
+    public abstract void endWrite();
+
+    public void run() {
+        while (true) {
+            startWrite();
+            System.out.println("Writer " + id + " is writing.");
+            Util.sleep(500, 1000);
+            endWrite();
+            Util.sleep(500, 1000);
+        }
+    }
+}
